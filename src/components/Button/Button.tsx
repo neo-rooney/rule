8abcdd8035080f78304e03a74447359e0bc2f051
rooney <bch3454@gmail.com>
2022-12-07 15:04:09 +0900

    [ build-react ] Feat: Created button component for test

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from "@mui/material";
+
+type ButtonBaseProps = Pick<MuiButtonProps, "variant" | "size" | "color">;
+
+export interface ButtonProps extends ButtonBaseProps {
+  label: string;
+}
+
+const Button = ({ label, ...rest }: ButtonProps) => (
+  <MuiButton {...rest}>{label}</MuiButton>
+);
+
+export default Button;
